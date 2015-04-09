1fc9510a7a4f661f747560757c77ae5300f31a9c
wendylwise@gmail.com <wendylwise@gmail.com>
2015-04-09 12:02:16 -0400

    chapters 8 and 9

---
+++ Ch9-Playground/Chapter9Playground.playground/contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var x = 5
+var y = 5
+var z = 15
+
+if (x < y) {
+    println("\(x) is less than \(y)")
+} else if (x > y){
+    println("\(x) is greater than \(y)")
+} else{
+    println("\(x) must be equal to \(y)")
+}
+
+
+
+
